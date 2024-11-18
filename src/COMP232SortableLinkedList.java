fe88de174c65a4feff3d0442d461f46520a0c69d
Phu Nguyen <nguyehuu@dickinson.edu>
2024-11-17 21:11:01 -0500

    done the hw

--- src/COMP232SortableLinkedList.java
+++ src/COMP232SortableLinkedList.java
@@ -14,8 +14,54 @@ public class COMP232SortableLinkedList<E extends Comparable<E>> extends COMP232D
 	 * list will be sorted according to the order imposed by their compareTo
 	 * method.
 	 */
+	//This is an optimized version of insertion sort, since we only have to "insert" the element once instead of swapping it multiple times.
+	//It is possible to go back to the "normal" version, simply by changing the inner body of the while loop.
 	public void insertionSortList() {
 		// Intentionally not implemented - see homework assignment.
-		throw new UnsupportedOperationException("Not yet implemented");
+
+		//cursor1 used to iterate forward. If the list have less than 2 element, it does not need to be sorted, hence cursor1 should be tail making the method do nothing.
+		DLLNode<E> cursor1 = super.tail;
+		if (super.size > 1) {
+			cursor1 = super.head.next.next;
+		}
+
+		//cursor2 used to iterate backward
+		DLLNode<E> cursor2;
+
+		//current is the node we are currently trying to put in the sorted part of the array
+		DLLNode<E> current;
+
+		while(cursor1 != super.tail){
+			current= cursor1;
+			cursor2=current.prev;
+			cursor1 = cursor1.next;
+
+			while(cursor2 != super.head && cursor2.element.compareTo(current.element) > 0 ){
+				boolean rightInsertSpot = (cursor2.prev == super.head  || cursor2.prev.element.compareTo(current.element) <= 0);
+
+				//insert the node in between cursor2 and cursor2.prev. Before that Node.prev will be connected to Node.next
+				if(rightInsertSpot){
+					current.prev.next = current.next;
+					current.next.prev = current.prev;
+
+					current.next = cursor2;
+					current.prev = cursor2.prev;
+					cursor2.prev.next=current;
+					cursor2.prev=current;
+
+				}
+
+				cursor2=cursor2.prev;
+
+
+				//below are the code that would be used for the body of the while loop if we want "normal" insertion sort
+//					E dummyVal = cursor2.element;
+//					cursor2.element=current.element;
+//					current.element=dummyVal;
+//
+//					current=current.prev;
+//					cursor2=cursor2.prev;
+			}
+		}
 	}
 }
--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,9 +1,49 @@
 import java.util.Arrays;
 
 public class HeapSort {
+
     public static void heapSort(Integer[] values) {
-        // Intentionally not implemented -- see homework assignmnet
-        throw new UnsupportedOperationException("Not yet implemented.");
+        // Intentionally not implemented -- see homework assignment
+        heapify(values); //O(n)
+        Integer dummyVal;
+        for (int i = values.length - 1; i > 0; i--) { //n*O(lgn) -> O(nlgn)
+            dummyVal = values[i];
+            values[i] = values[0];
+            values[0] = dummyVal;
+            trickleDown(values,0,i-1);
+        }
+    }
+
+    //O(n)
+    private static void heapify(Integer[] values){
+        for (int i = values.length/2 - 1; i >= 0; i--){
+           trickleDown(values,i,values.length);
+        }
+    }
+
+    //O(lgn)
+    private static void trickleDown(Integer[] values, int index, int heapSize){
+        int leftChildIndex = index*2+1;
+        int rightChildIndex = index*2+2;
+        int smallerChildIndex;
+        if(leftChildIndex < heapSize && rightChildIndex < heapSize){
+            smallerChildIndex = values[leftChildIndex]<values[rightChildIndex] ? leftChildIndex : rightChildIndex;
+        }
+        else if(leftChildIndex < heapSize){
+            smallerChildIndex = leftChildIndex;
+        }
+        else if(rightChildIndex < heapSize){
+            smallerChildIndex = rightChildIndex;
+        }
+        else{
+            return;
+        }
+        if(values[index] > values[smallerChildIndex]){
+            Integer dummyVal = values[index];
+            values[index] = values[smallerChildIndex];
+            values[smallerChildIndex] = dummyVal;
+            trickleDown(values, smallerChildIndex, heapSize);
+        }
     }
 
     public static void main(String[] args) {
